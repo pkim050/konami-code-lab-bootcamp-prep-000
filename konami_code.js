988614c8c7dc055c4ec4d83f428e192dd42f1e01
Patrick A Kim <pkim050@gmail.com>
2018-11-28 04:21:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  var counter = 0;
+  const key = document.addEventListener('keydown', init);
+  //const key2 = key.key;
+  for (var i = 0; i < codes.length; i++) {
+    if (key === codes[counter]) {
+      counter++
+      if (counter === codes.length) {
+        alert("Congratulations, you have correctly inputed the cheat code!");
+      }
+    }
+    else {
+      i = 0;
+      counter = 0;
+    }
+  }
 }
